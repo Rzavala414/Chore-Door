809af74f791155cefb318f614f7a8a5b8538d0b4
Roger Zavala <rogeliozavala15@gmail.com>
2020-01-25 15:42:26 -0800

    created three new onclick functions that change to either a space door or beach door

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let doorImage1 = document.getElementById("door1");
-let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3")
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
 door1.onclick = () =>{
     doorImage1.src = botDoorPath;
+}
+door2.onclick = () =>{
+    doorImage2.src = beachDoorPath;
+}
+door3.onclick = () =>{
+    doorImage3.src = spaceDoorPath;
 }
