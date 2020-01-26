ee85d5937e55afdcef7613df75c0bbe64b1aff9e
Roger Zavala <rogeliozavala15@gmail.com>
2020-01-25 19:26:37 -0800

    created a working function to randomize our door images

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 let doorImage1 = document.getElementById("door1");
+
 let doorImage2 = document.getElementById("door2");
+
 let doorImage3 = document.getElementById("door3")
+
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
+let numClosedDoors = 3;
+
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () =>{
+    const choreDoor = Math.floor(Math.random() * numClosedDoors )
+    if(choreDoor === 2){
+        openDoor1 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+    } else if(choreDoor === 1){
+        openDoor2 = botDoorPath;
+        openDoor1 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+    } else{
+        openDoor3 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor1 = spaceDoorPath;
+    }
+
+}
+
 door1.onclick = () =>{
-    doorImage1.src = botDoorPath;
+    doorImage1.src = openDoor1;
 }
 door2.onclick = () =>{
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 }
 door3.onclick = () =>{
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
 }
+
